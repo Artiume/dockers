229574abd54f4c389c37d043282be59bacf91031
Anthony PERIQUET <anthony@periquet.net>
2018-06-27 16:17:47 +0200

    Add passive configuration (Host, Port range)

--- pure-ftpd/1.0/docker-entrypoint.sh
+++ pure-ftpd/1.0/docker-entrypoint.sh
@@ -33,10 +33,23 @@ if [ "${DEFAULT_USR}" != "username" ]; then
 	pure-pw mkdb /opt/pure/secure/pureftpd.pdb
 fi
 
+FTP_FLAGS="-0 clf:/dev/stdout"
+if [ -n "${FTP_PASSIVE_PORT_RANGE}" ]; then
+	if [ -z "${FTP_PASSIVE_HOST}" ]; then
+		echo "Usage :"
+		echo " - FTP_PASSIVE_HOST must be defined"
+		exit
+	fi
+	FTP_FLAGS="${FTP_FLAGS} -P ${FTP_PASSIVE_HOST} -p ${FTP_PASSIVE_PORT_RANGE}"
+fi
+
+echo "Starting Pure-FTPD :"
+echo "> pure-ftpd -c ${FTP_CLIENTS} -C ${FTP_CONNECTIONS} -l puredb:/opt/pure/secure/pureftpd.pdb -j -E ${FTP_FLAGS}"
+
 # Option explanations :
 #	-c <int> : Allow maximum of clients to be connected
 #	-C <int> : Limit the number of simultaneous connections coming from the same IP address.
 #	-E : Only allow authenticated users. Anonymous logins are prohibited.
 #	-j : If the home directory of a user doesn't exist, automatically create it.
 #	-l <puredb:path> : PureDB file
-exec /usr/sbin/pure-ftpd -c 5 -C 5 -l puredb:/opt/pure/secure/pureftpd.pdb -j -E
+exec /usr/sbin/pure-ftpd -c ${FTP_CLIENTS} -C ${FTP_CONNECTIONS} -l puredb:/opt/pure/secure/pureftpd.pdb -j -E $FTP_FLAGS

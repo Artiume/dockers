34d808d5bd6416b769ebca2635be7b6903e28335
Anthony PERIQUET <anthony@periquet.net>
2018-06-28 16:35:30 +0200

    Remove comment

--- pure-ftpd/1.0/docker-entrypoint.sh
+++ pure-ftpd/1.0/docker-entrypoint.sh
@@ -35,7 +35,7 @@ fi
 
 FTP_FLAGS=""
 if [ -n "${FTP_HOST}" ]; then
-    FTP_FLAGS="${FTP_FLAGS} -S ${FTP_HOST},"
+    FTP_FLAGS="${FTP_FLAGS} -P ${FTP_HOST}"
 fi
 if [ -n "${FTP_PASSIVE_PORT_RANGE}" ]; then
     if [ -z "${FTP_HOST}" ]; then
@@ -60,4 +60,4 @@ echo "> pure-ftpd -c ${FTP_CLIENTS} -C ${FTP_CONNECTIONS} -l puredb:/opt/pure/se
 #   -l <puredb:path> : PureDB file
 #   -P <ip address or host name> : Force the specified parameter in reply to a PASV/EPSV/SPSV command.
 #   -p <rangeStart:rangeStop> : Only use ports in range <rangeStart:rangeStop> for passive mode downloads.
-exec /usr/sbin/pure-ftpd -c ${FTP_CLIENTS} -C ${FTP_CONNECTIONS} -l puredb:/opt/pure/secure/pureftpd.pdb -j -E -H ${FTP_FLAGS} $@
+exec /usr/sbin/pure-ftpd -c ${FTP_CLIENTS} -C ${FTP_CONNECTIONS} -l puredb:/opt/pure/secure/pureftpd.pdb -j -E -H${FTP_FLAGS} $@
